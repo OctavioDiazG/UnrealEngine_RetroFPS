0f31688e9e4150851c75542f3b0eb2672c9ed1c1
OctavioDiazG <odiazg01@outlook.com>
2023-09-21 19:02:32 -0600

    Start Retro FPS Game

---
+++ Source/RetroFPS/RetroFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RetroFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RETROFPS_API ARetroFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
